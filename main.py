c41c5d55130a6a44ef0365ca5e9c54da50b356eb
Loandr1324 <90257116+Loandr1324@users.noreply.github.com>
2021-10-12 01:21:54 +1000

    Добавил поиск файлов в директории

---
+++ main.py
@@ -0,0 +1,147 @@
+# TODO
+#  Часть 1
+#  1. Создать отдельный файл программы для чтения файлов эксель. Передавать туда маску наименования файла, который требуется прочитать.
+#  2. Прочитать все файлы по продажам и объединить
+#  3. Прочитать все файлы МО и объединить.
+#  4. Прочиттаь старый итоговый файл и взять от туда старые данные по продажам
+#  5. Занести все данные в новый файл и найти отличия новых данных по продажам по всей компании со старыми и записать в отдельную колонку
+#  6. Отформатировать все строки, выделить те строки в которых в колонке Отличие значение не равно нулю
+#  Часть 2
+#  7. Считать значения МО из эксель файл полученного после редактирования отвественным заказчиком итого вого файла из Части 1
+#  8. Создать файл для загрузки значений МО, которые были считаны в п.7
+
+import pandas as pd
+import os
+# import readfile
+
+FOLDER = 'Установка МО ответственным'
+salesName = 'продажи'
+minStockName = 'МО'
+# sales = readxls(FOLDER, 'Продажи')
+
+def search_file( name ):
+    """
+    Поиск всех файла в наименовании которых содержится name
+    :return: filelist список с наименованиями фалов
+    """
+    filelist = []
+    for item in os.listdir(FOLDER):  # для каждого файла в папке FOLDER
+        if name in item and item.endswith( '.xlsx' ):
+            filelist.append ( FOLDER + "/" + item )
+        else:
+            pass
+    return filelist
+
+def read_xlsx( file_list ):
+    """
+    Загружаем в DataFrame файлы из file_list
+    :param file_list:
+    :return: df Дата фрэйм с данными из файлов
+    """
+    '''
+    df = [pd.read_excel(filename, sheet_name='TDSheet', header=0, usecols="A:G",
+                   skipfooter=1, engine='openpyxl') for filename in file_list]
+    '''
+    df = pd.concat([pd.read_excel(filename, sheet_name='TDSheet', header=0, usecols="A:M",
+                                  skipfooter=1, engine='openpyxl') for filename in file_list], ignore_index=True)
+
+    dfHeader = df.iloc[:15, :1]
+    mask = dfHeader == 'Номенклатура.Код'
+    print (mask)
+    print ( dfHeader[mask].any() )
+
+    # print ( mask.loc[lambda x: x == True].index )
+
+
+    # print ( dfHeader.index[dfHeader == 'Номенклатура.Код'].tolist() )
+
+
+    '''
+    i = 0
+    for col in dfHeader:
+        for row in dfHeader[col]:
+            print ( row )
+            # if 'Номенклатура' in row:
+            #    print ( i, row )
+    '''
+    '''
+    df = pd.concat([pd.read_excel(filename, sheet_name='TDSheet', header=9, usecols="A:M",
+                                          skipfooter=1, engine='openpyxl') for filename in file_list], ignore_index=True)
+    df = df.dropna(axis=1, how='all')  # Удаление пустых колонок, если axis=0, то строк
+    # custom_row = custom_row.dropna(axis=0, how='any')  # Удаление строк в которых есть хоть одно пустое значение, axis=1
+    '''
+
+    pass
+
+    return
+
+
+
+if __name__ == '__main__':
+
+    salesFilelist = search_file ( salesName )
+    minStockFilelist = search_file ( minStockName )
+    # print ( salesFilelist )
+    # print ( minStockFilelist )
+    read_xlsx ( salesFilelist )
+
+    '''
+    Блок №1
+    Чтение данных за прошедший месяц, сортировка и запись суммовых значений в итоговый файл с данными
+    
+    print("Начало Блока №1")
+    # Поиск списков файлов для чтения и распределение по типам файлов
+    file_custom_order, file_supp_order, file_supp_receipt, file_sms = search_file()
+    # Определение старых и новых имён файлов с хранимыми данным и переименование
+    file_custom, file_supp = rename_out_file()
+    # Чтение данных за прошедший месяц по заказам клиента и запись в DataFrame + получения общего количетсва строк
+    custom_row, sms_row, quantity_row_custom = read_xlsx_custom(file_custom_order, file_sms)
+    # Сортировка DataFrame заказов клиента по типам продаж
+    your_warehouse, another_warehouse, by_order = sorting_custom_row(custom_row)
+    # Сортировка DataFrame СМС. Добавляем, если доставлено
+    sms = sorting_sms(sms_row)
+    # Чтение данных за прошедший месяц по заказам поставщикам и запись в DataFrame + получения общего количетсва строк
+    supp_ord_row, supp_rec_row, quantity_row_supp = read_xlsx_supp(file_supp_order, file_supp_receipt)
+    # Получение итоговых значений по типам продаж и объединение итоговых значений в один DataFrame
+    total_custom = total_df_custom(another_warehouse, your_warehouse, by_order, sms)
+    # Получение итоговых значений по типам дкументов и объединение итоговых значений в один DataFrame
+    total_supp = total_df_supp(supp_ord_row, supp_rec_row)
+    # Добавляем в файл данных по клиентам, данные по Заказам клиента
+    append_file_data(file_custom, total_custom)
+    # Добавляем в файл данных по поставщикам, данные по Заказам поставщикам
+    append_file_data(file_supp, total_supp)
+    print("Блок №1 завершён!")
+    
+    Блок №2
+    Чтение данных из файлов данных, построение графиков и запись итоговых файлов для отпрвки по почте
+    
+    # Считываем данные за всё время и формируем итоговый файл по Заказам для наличия
+    print("Начало Блока №2")
+    # print(file_custom, file_supp) # Используем при тестах
+    custom_category = 'Тип продажи'
+    custom_set_cat = ['Продажи с других складов', 'Продажи со своего склада',
+                      'Заказное', 'Отправка СМС', 'Итого за год']
+    custom_index = ['Год', 'Тип продажи', 'Месяц']
+    supp_category = 'Тип документа'
+    supp_set_cat = ['Заказы внешним поставщикам', 'Поступления от МХ Комсомольск', 'Итого за год']
+    supp_index = ['Год', 'Тип документа', 'Месяц']
+    supp_index_out = ['Год', 'Месяц', 'Тип документа']
+    caption_custom = 'Carbaz Заказы клиентов. Все заказы кроме наших заказов внешним поставщикам на ' \
+                     'постоянное наличие. Включая индивидуальные заказы.'
+    caption_supp = 'Наши заказы внешним поставщикам на постоянное наличие. (Заказы Клиентов не входят)'
+    sec_level_custom = ['Год', 'Тип продажи']
+    sec_level_supp = ['Год', 'Месяц']
+    data_pt_custom = pivot_table(file_custom, custom_category, custom_set_cat, custom_index)
+    data_pt_supp = pivot_table(file_supp, supp_category, supp_set_cat, supp_index)
+    result_to_xlsx(out_file_custom, data_pt_custom, custom_category, caption_custom, sec_level_custom, custom_index)
+    result_to_xlsx(out_file_supp, data_pt_supp, supp_category, caption_supp, sec_level_supp, supp_index_out)
+    print("Блок №2 завершён!")
+    
+    Блок №3
+    Отправка файлов по почте и выдержка по количеству строк в теле письма
+    
+    # Подготавливаем и отправляем данные по почте Юре
+    print("Начало Блока №3")
+    send_mail()
+    print("Блок №3 завершён!")
+    '''
